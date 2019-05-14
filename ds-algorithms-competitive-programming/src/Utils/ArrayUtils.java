9cccfd04168f2a9bc8f68cf3563dc7df003c75c0
Tanmay <tanmayawasthi105@gmail.com>
2019-05-14 23:18:11 +0530

    more problems added

--- ds-algorithms-competitive-programming/src/Utils/ArrayUtils.java
+++ ds-algorithms-competitive-programming/src/Utils/ArrayUtils.java
@@ -3,6 +3,16 @@ package Utils;
 import java.io.PrintStream;
 
 public class ArrayUtils {
+
+    public static void print2DArray(char[][] T) {
+        for (int i = 0; i < T.length; i++) {
+            for (int j = 0; j < T[i].length; j++) {
+                System.out.print("T[" + i + "," + j + "] " + T[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
     public static void print2DArray(int[][] T) {
         for (int i = 0; i < T.length; i++) {
             for (int j = 0; j < T[i].length; j++) {
