be9c91af193999f63629d479931aa34cf3962cef
Tanmay <tanmayawasthi105@gmail.com>
2019-05-17 22:15:51 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/ContainerWithMostWater.java
@@ -0,0 +1,39 @@
+package competitiveProgramming.leetcode;
+
+import Utils.ArrayUtils;
+import Utils.LoggingUtil;
+
+//https://leetcode.com/problems/container-with-most-water/
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] input = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(input));
+    }
+
+    public static int maxArea(int[] height) {
+        ArrayUtils.printArray(height);
+        int overallMax = 0;
+        int maxAreaForIndex = 0;
+        for (int i = 0; i < height.length; i++) {
+            maxAreaForIndex = getMaxAreaForX(i, height);
+            if (maxAreaForIndex > overallMax) {
+                overallMax = maxAreaForIndex;
+            }
+        }
+        return overallMax;
+    }
+
+    private static int getMaxAreaForX(int currentIndex, int[] height) {
+        int currentIndexArea;
+        int maxAreaForIndex = 0;
+        int indexHeight = height[currentIndex];
+        for (int i = currentIndex + 1; i < height.length; i++) {
+            currentIndexArea = Math.min(height[i], indexHeight) * (i - currentIndex);
+            if (currentIndexArea > maxAreaForIndex) {
+                maxAreaForIndex = currentIndexArea;
+
+            }
+        }
+        return maxAreaForIndex;
+    }
+}
---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/PalindromeNumber.java
@@ -0,0 +1,30 @@
+package competitiveProgramming.leetcode;
+
+import Utils.LoggingUtil;
+
+//https://leetcode.com/problems/palindrome-number/
+class PalindromeNumber {
+
+    public static void main(String[] args) {
+        int num = 123;
+        System.out.println(isPalindrome(num));
+    }
+
+    private static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        if (x < 10) {
+            return true;
+        }
+        int original = x;
+        int rev_num = 0;
+        while (x > 0) {
+            rev_num = rev_num * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return original == rev_num;
+    }
+}
