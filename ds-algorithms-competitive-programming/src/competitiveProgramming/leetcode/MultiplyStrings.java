b4e37306261716b98760593e1eddb428d3ce5ef7
Tanmay <tanmayawasthi105@gmail.com>
2019-06-12 23:19:33 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/MultiplyStrings.java
@@ -0,0 +1,69 @@
+package competitiveProgramming.leetcode;
+
+import java.util.ArrayList;
+
+//https://leetcode.com/problems/multiply-strings/
+
+//Tag: Not complete but working
+public class MultiplyStrings {
+
+    /**
+     *
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println(multiply("12", "456"));
+    }
+
+    public static String multiply(String num1, String num2) {
+        char[] num1Char = num1.toCharArray();
+        char[] num2Char = num2.toCharArray();
+        Integer sum = 0;
+        ArrayList<StringBuilder> levels = new ArrayList<>();
+        StringBuilder level;
+
+        int carry = 0;
+        int zeros = 0;
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            level = new StringBuilder();
+//            System.out.println("<-----------------" + i + "-------------------->");
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                sum = (num1Char[i] - '0') * (num2Char[j] - '0') + carry;
+                if (sum > 10) {
+                    carry = sum / 10;
+                    level.append(sum % 10);
+                } else {
+                    level.append(sum);
+                    carry = 0;
+                }
+//                LoggingUtil.logNewLine(num1Char[i], num2Char[j]);
+            }
+            if (carry > 0) {
+                level.append(carry);
+                carry = 0;
+            }
+            appendZeros(level.reverse(), zeros);
+            zeros++;
+            levels.add(level);
+        }
+//        System.out.println(levels);
+        return computeSum(levels);
+    }
+
+    private static String computeSum(ArrayList<StringBuilder> levels) {
+        Integer sum = 0;
+        for (StringBuilder level : levels) {
+            sum += Integer.parseInt(level.toString());
+        }
+
+        return sum.toString();
+    }
+
+    private static StringBuilder appendZeros(StringBuilder level, int j) {
+        for (int i = 0; i < j; i++) {
+            level.append(0);
+        }
+        return level;
+    }
+
+}
---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/RemoveRepeatingNumbers.java
@@ -0,0 +1,41 @@
+package competitiveProgramming.leetcode;
+
+import Utils.ArrayUtils;
+import Utils.LoggingUtil;
+
+import java.util.ArrayList;
+
+//https://leetcode.com/discuss/interview-question/309064/google-phone-interview-remove-repeating-numbers
+public class RemoveRepeatingNumbers {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 1, 2, 2, 2, 1, 1, 1, 2, 2, 3, 1, 1, 2, 2, 2, 1, 1, 1, 2, 3};
+        ArrayUtils.printArray(removeDuplicateOrderN(arr));
+    }
+
+    private static Object[] removeDuplicateOrderN(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+        int prev = -1;
+        int next = -1;
+        int curr = -1;
+        boolean checking = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            next = arr[i + 1];
+            curr = arr[i];
+            if (prev != arr[i]) {
+                if (arr[i] != next) {
+                    list.add(curr);
+                }
+            }
+            prev = arr[i];
+        }
+
+        if (next != curr) {
+            list.add(next);
+        }
+
+        return list.toArray();
+    }
+}
