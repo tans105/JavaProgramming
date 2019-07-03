5687bbd3baabae9507509987a06d9c74f1bd4e86
Tanmay <tanmayawasthi105@gmail.com>
2019-07-03 22:40:57 +0530

    more problems

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/MinimumPathSum.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/MinimumPathSum.java
@@ -1,5 +1,7 @@
 package competitiveProgramming.leetcode;
 
+import Utils.ArrayUtils;
+
 public class MinimumPathSum {
 
     public static void main(String[] args) {
@@ -9,6 +11,7 @@ public class MinimumPathSum {
                 {4, 2, 1}
         };
         System.out.println(minPathSum(grid));
+        System.out.println(minPathSumDP(grid));
     }
 
     public static int minPathSum(int[][] grid) {
@@ -23,7 +26,7 @@ public class MinimumPathSum {
         } else if (x == xLength - 1 && y == yLength - 1) {
             return grid[x][y];
         } else {
-            return grid[x][y] + min(minPathSumRec(grid, x + 1, y, xLength, yLength), minPathSumRec(grid, x, y + 1, xLength, yLength), minPathSumRec(grid, x + 1, y + 1, xLength, yLength));
+            return grid[x][y] + min(minPathSumRec(grid, x + 1, y, xLength, yLength), minPathSumRec(grid, x, y + 1, xLength, yLength), Integer.MAX_VALUE);
         }
     }
 
@@ -32,5 +35,36 @@ public class MinimumPathSum {
             return (x < z) ? x : z;
         else
             return (y < z) ? y : z;
+    }
+
+    static int minPathSumDP(int[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+
+        if (grid.length == 0) {
+            return 0;
+        }
+        int xLength = grid.length;
+        int yLength = grid[0].length;
+        int[][] arr = new int[xLength][yLength];
+        arr[0][0] = grid[0][0];
+        //vertical
+        for (int i = 1; i < xLength; i++) {
+            arr[i][0] = arr[i - 1][0] + grid[i][0];
+        }
+
+        //horizontal
+        for (int i = 1; i < yLength; i++) {
+            arr[0][i] = arr[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < xLength; i++) { //x
+            for (int j = 1; j < yLength; j++) { //y
+                arr[i][j] = grid[i][j] + Math.min(arr[i - 1][j], arr[i][j - 1]);
+            }
+        }
+
+        return arr[xLength - 1][yLength - 1];
     }
 }
