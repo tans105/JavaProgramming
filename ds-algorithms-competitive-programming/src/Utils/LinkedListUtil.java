f367b6b665a580d04c21d4e952e7b10e8f6f3e93
Tanmay <tanmayawasthi105@gmail.com>
2019-07-17 21:11:34 +0530

    more problems

--- ds-algorithms-competitive-programming/src/Utils/LinkedListUtil.java
+++ ds-algorithms-competitive-programming/src/Utils/LinkedListUtil.java
@@ -1,6 +1,8 @@
 package Utils;
 
 public class LinkedListUtil {
+    public static final String LL_SEPARATOR = " -> ";
+
     public static ListNode populateList(int[] arr, ListNode head) {
         ListNode n = head;
         ListNode head1 = n;
@@ -12,11 +14,25 @@ public class LinkedListUtil {
         return head1;
     }
 
+    public static ListNode populateList(int[] arr) {
+        if (arr.length == 0) return null;
+        ListNode n = new ListNode(arr[0]);
+        ListNode head1 = n;
+        for (int i = 1; i < arr.length; i++) {
+            n.next = new ListNode(arr[i]);
+            n = n.next;
+        }
+
+        return head1;
+    }
+
     public static void printList(ListNode head) {
         ListNode n = head;
         while (n != null) {
-            System.out.println(n.val + "\t");
+            System.out.print(n.val + LL_SEPARATOR);
             n = n.next;
         }
+
+        System.out.print("null");
     }
 }
