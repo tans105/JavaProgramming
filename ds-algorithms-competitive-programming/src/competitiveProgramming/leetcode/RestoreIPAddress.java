391888abbb2efb63c79ed3f3ad22938217340e73
Tanmay <tanmayawasthi105@gmail.com>
2019-07-21 13:28:59 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/RestoreIPAddress.java
@@ -0,0 +1,50 @@
+package competitiveProgramming.leetcode;
+
+import Utils.LoggingUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 93. Restore IP Addresses
+ * Medium
+ * <p>
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ * <p>
+ * Example:
+ * <p>
+ * Input: "25525511135"
+ * Output: ["255.255.11.135", "255.255.111.35"]
+ */
+public class RestoreIPAddress {
+    public static void main(String[] args) {
+        System.out.println(restoreIpAddresses("0000"));
+    }
+
+    public static List<String> restoreIpAddresses(String s) {
+        List<String> ans = new ArrayList<>();
+        int len = s.length();
+
+        for (int i = 0; i < 4 && i < len - 2; i++) {
+            for (int j = i + 1; j < i + 4 && j < len - 1; j++) {
+                for (int k = j + 1; k < j + 4 && k < len; k++) {
+                    String s1 = s.substring(0, i);
+                    String s2 = s.substring(i, j);
+                    String s3 = s.substring(j, k);
+                    String s4 = s.substring(k, len);
+                    if (isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)) {
+                        ans.add(s1 + "." + s2 + "." + s3 + "." + s4);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private static boolean isValid(String s) {
+        if (s.length() > 3 || s.length() == 0 || (s.charAt(0) == '0' && s.length() > 1) || Integer.parseInt(s) > 255)
+            return false;
+        return true;
+    }
+}
