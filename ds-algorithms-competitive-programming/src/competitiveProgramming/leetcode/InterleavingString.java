6cd8817b1e47db23cc64e983b8932d46bf726517
Tanmay <tanmayawasthi105@gmail.com>
2019-07-23 22:59:57 +0530

    Optimised interleaving string using memoization

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/InterleavingString.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/InterleavingString.java
@@ -1,9 +1,5 @@
 package competitiveProgramming.leetcode;
 
-import Utils.LoggingUtil;
-
-import java.util.Stack;
-
 /**
  * 97. Interleaving String
  * Hard
@@ -25,35 +21,36 @@ public class InterleavingString {
         System.out.println(isInterleave(s1, s2, s3));
     }
 
-    public static boolean isInterleave(String s1, String s2, String s3) {
-        isInterleave(s1, 0, s2, 0, s3, 0);
-        return is_Interleave(s1, 0, s2, 0, "", s3);
+    private static boolean isInterleave(String s1, String s2, String s3) {
+        int[][] memo = new int[s1.length()][s2.length()];
+        for (int i = 0; i < s1.length(); i++) {
+            for (int j = 0; j < s2.length(); j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return isInterleave(s1, 0, s2, 0, s3, 0, memo);
     }
 
-    private static boolean isInterleave(String s1, int i1, String s2, int i2, String s3, int i3) {
-        if (i1 == s1.length() - 1 && i2 == s2.length() - 1 && i3 == s3.length() - 1) {
-            return true;
+
+    private static boolean isInterleave(String s1, int i, String s2, int j, String s3, int k, int[][] memo) {
+        if (i == s1.length()) {
+            return s2.substring(j).equals(s3.substring(k));
         }
-        LoggingUtil.logNewLine(i1, i2, i3);
-        boolean ans = false;
-        if (i1 < s1.length()) {
-            ans |= isInterleave(s1, i1 + 1, s2, i2, s3, i3 + 1);
-        } else if (i2 < s2.length()) {
-            ans |= isInterleave(s1, i1, s2, i2 + 1, s3, i3 + 1);
+        if (j == s2.length()) {
+            return s1.substring(i).equals(s3.substring(k));
         }
 
-        return ans;
-    }
-
-    public static boolean is_Interleave(String s1, int i, String s2, int j, String res, String s3) {
-        if (res.equals(s3) && i == s1.length() && j == s2.length())
-            return true;
+        if (memo[i][j] >= 0) {
+            return memo[i][j] == 1;
+        }
         boolean ans = false;
-        LoggingUtil.logNewLine(i, j, res);
-        if (i < s1.length())
-            ans |= is_Interleave(s1, i + 1, s2, j, res + s1.charAt(i), s3);
-        if (j < s2.length())
-            ans |= is_Interleave(s1, i, s2, j + 1, res + s2.charAt(j), s3);
+
+        if (s3.charAt(k) == s1.charAt(i) && isInterleave(s1, i + 1, s2, j, s3, k + 1, memo)
+                || s3.charAt(k) == s2.charAt(j) && isInterleave(s1, i, s2, j + 1, s3, k + 1, memo)) {
+            ans = true;
+        }
+
+        memo[i][j] = ans ? 1 : 0;
         return ans;
 
     }
