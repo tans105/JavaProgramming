4e30b5f5ac30db0aabeed609fbb0ccfdaddbc83e
Tanmay <tanmayawasthi105@gmail.com>
2019-07-27 18:39:49 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/Utils/TreeNode.java
@@ -0,0 +1,13 @@
+package Utils;
+
+public class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+    public int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
