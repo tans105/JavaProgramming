522cc6d01371d3756c13fa6aafda011433d4c3f8
Tanmay <tanmayawasthi105@gmail.com>
2019-08-10 16:58:03 +0530

    more problems

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/LevelOrderTraversal.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/LevelOrderTraversal.java
@@ -6,6 +6,26 @@ import utils.pojo.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+102. Binary Tree Level Order Traversal
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ */
+
 public class LevelOrderTraversal {
     public static void main(String[] args) {
         TreeNode node = TreeUtils.generateBinaryTreeFromArray(new Integer[]{3, 9, 20, null, null, 15, 7});
