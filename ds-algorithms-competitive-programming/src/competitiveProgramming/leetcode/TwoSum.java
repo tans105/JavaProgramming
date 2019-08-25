fb869734d67067d2880c6a4facdbeeae2a7e6d39
Tanmay <tanmayawasthi105@gmail.com>
2019-08-25 12:56:12 +0530

    More problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/TwoSum.java
@@ -0,0 +1,39 @@
+package competitiveProgramming.leetcode;
+
+import utils.ArrayUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+1. Two Sum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtils.parseArray("[2, 7, 11, 15]");
+        ArrayUtils.printArray(twoSum(arr, 9));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
