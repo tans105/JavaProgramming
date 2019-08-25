ced40c910d684f64e097a2bb194697069b6f8513
Tanmay <tanmayawasthi105@gmail.com>
2019-08-25 19:52:38 +0530

    code refactor

--- ds-algorithms-competitive-programming/src/miniApplications/myCustomList/MyCustomList.java
+++ ds-algorithms-competitive-programming/src/miniApplications/myCustomList/MyCustomList.java
@@ -60,4 +60,23 @@ public class MyCustomList {
         builder.append(']');
         return builder.toString();
     }
+
+    /**
+     * Runner
+     * @param args
+     */
+    public static void main(String[] args) {
+        MyCustomList list = new MyCustomList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println(list);
+        System.out.println(list.get(2));
+        System.out.println(list.size());
+        list.remove(0);
+        System.out.println(list);
+        System.out.println(list.get(0));
+        System.out.println(list.size());
+    }
 }
