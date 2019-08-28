1bfd40686456845c5950478ae383e0876e67bed8
Tanmay <tanmayawasthi105@gmail.com>
2019-08-28 21:54:53 +0530

    more problems

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/MinimumDepthOfBinaryTree.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/MinimumDepthOfBinaryTree.java
@@ -44,4 +44,12 @@ public class MinimumDepthOfBinaryTree {
             minDepthHelper(root.right, count + 1);
         }
     }
+
+    public static int minDepth2(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+
+    }
 }
