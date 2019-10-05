b2ea89222b9c9fbb702a887c868387647bd797c2
Tanmay <tanmayawasthi105@gmail.com>
2019-10-05 14:14:44 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/InsertionSortList.java
@@ -0,0 +1,76 @@
+package competitiveProgramming.leetcode;
+
+/*
+147. Insertion Sort List
+
+Sort a linked list using insertion sort.
+
+Algorithm of Insertion Sort:
+
+Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list.
+At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there.
+It repeats until no input elements remain.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+ */
+
+import utils.LinkedListUtil;
+import utils.pojo.ListNode;
+
+public class InsertionSortList {
+    public static void main(String[] args) {
+        ListNode root = LinkedListUtil.populateList(new int[]{-1, 5, 3, 4, 0});
+        LinkedListUtil.printList(insertionSortList(root));
+    }
+
+    private static ListNode insertionSortList(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        ListNode current = head.next;
+        ListNode prev = head;
+        while (current != null) {
+            if (current.val < prev.val) {
+                //Removing the current from the list
+                ListNode nextNode = current.next;
+                prev.next = current.next;
+                current.next = null;
+
+                //Adding the current back
+                head = findSpotAndInsert(head, current);
+                current = nextNode;
+            } else {
+                prev = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    private static ListNode findSpotAndInsert(ListNode head, ListNode current) {
+        ListNode n = head;
+        ListNode prev = null;
+        if (current.val < head.val) {
+            current.next = head;
+            return current;
+        } else {
+            while (n != null) {
+                if (prev != null && n.val > current.val) {
+                    prev.next = current;
+                    current.next = n;
+                    break;
+                }
+                prev = n;
+                n = n.next;
+            }
+        }
+        return head;
+    }
+}
--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/LRUCache.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/LRUCache.java
@@ -2,6 +2,35 @@ package competitiveProgramming.leetcode;
 
 import java.util.HashMap;
 
+/*
+146. LRU Cache
+
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2   ); //capacity
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        cache.get(2);       // returns -1 (not found)
+        cache.put(4, 4);    // evicts key 1
+        cache.get(1);       // returns -1 (not found)
+        cache.get(3);       // returns 3
+        cache.get(4);       // returns 4
+
+ */
+
 public class LRUCache {
 
     private int capacity;
@@ -72,9 +101,9 @@ class DoublyLinkedList {
 
     public static void main(String[] args) {
         DoublyLinkedList dll = new DoublyLinkedList();
-        dll.addAtStart(new DLLNode(1,1));
-        dll.addAtStart(new DLLNode(2,2));
-        dll.addAtStart(new DLLNode(3,3));
+        dll.addAtStart(new DLLNode(1, 1));
+        dll.addAtStart(new DLLNode(2, 2));
+        dll.addAtStart(new DLLNode(3, 3));
         dll.print();
         dll.removeFromEnd();
         dll.print();
@@ -166,6 +195,6 @@ class DLLNode {
 
     @Override
     public String toString() {
-        return "[value=" + value + ", key=" + key +"]";
+        return "[value=" + value + ", key=" + key + "]";
     }
 }
