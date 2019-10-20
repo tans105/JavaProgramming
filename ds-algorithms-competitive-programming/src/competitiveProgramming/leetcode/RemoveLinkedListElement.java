61978dbcb3f4f7f66660509a3f333c0615bdba91
Tanmay <tanmayawasthi105@gmail.com>
2019-10-20 15:56:06 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/RemoveLinkedListElement.java
@@ -0,0 +1,50 @@
+package competitiveProgramming.leetcode;
+
+import datastructures.linkedList.linkList.LinkedList;
+import utils.LinkedListUtil;
+import utils.pojo.ListNode;
+
+/*
+203. Remove Linked List Elements
+
+Remove all elements from a linked list of integers that have value val.
+
+Example:
+
+Input:  1->2->6->3->4->5->6, val = 6
+Output: 1->2->3->4->5
+ */
+public class RemoveLinkedListElement {
+    public static void main(String[] args) {
+        ListNode node = LinkedListUtil.populateList(new int[]{6, 1, 2, 6, 3, 4, 5, 6});
+        ListNode head = removeElements(node, 6);
+        LinkedListUtil.printList(head);
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        ListNode n = head;
+        ListNode prev = null;
+
+        while (n != null) {
+            if (n.val == val) {
+                if (prev == null) {
+                    ListNode temp = n;
+                    n = n.next;
+                    temp.next = null;
+                    head = n;
+                    continue;
+                } else {
+                    ListNode temp = n;
+                    n = n.next;
+                    prev.next = temp.next;
+                    temp.next = null;
+                    continue;
+                }
+            }
+            prev = n;
+            n = n.next;
+        }
+
+        return head;
+    }
+}
