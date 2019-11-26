75a690eb3c26fb180bee2cbad8ce64a7d91baaf3
Tanmay <tanmayawasthi105@gmail.com>
2019-11-26 22:58:13 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/random/CheckBase64.java
@@ -0,0 +1,22 @@
+package competitiveProgramming.random;
+
+
+import java.util.Base64;
+
+public class CheckBase64 {
+    public static void main(String[] args) {
+        CheckBase64 base = new CheckBase64();
+        System.out.println(base.isBase64(""));
+    }
+
+    public boolean isBase64(String str) {
+        if(str == null || str.length() == 0) return false;
+
+        try {
+            Base64.getDecoder().decode(str);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+}
