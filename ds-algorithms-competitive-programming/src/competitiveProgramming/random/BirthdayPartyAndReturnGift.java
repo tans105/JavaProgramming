854e456ef4cd9a1c012f2376cdd268a98304557c
Tanmay <tanmayawasthi105@gmail.com>
2019-12-01 14:31:27 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/random/BirthdayPartyAndReturnGift.java
@@ -0,0 +1,11 @@
+package competitiveProgramming.random;
+
+public class BirthdayPartyAndReturnGift {
+    public static void main(String[] args) {
+        System.out.println(findDamaged(7, 5, 3));
+    }
+
+    public static int findDamaged(int N, int T, int D) {
+        return D + (T % N - 1);
+    }
+}
