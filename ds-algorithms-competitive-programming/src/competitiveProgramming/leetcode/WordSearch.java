c0f1708fff2694f6bac6f7467f32e6c0386f7903
Tanmay <tanmayawasthi105@gmail.com>
2019-12-05 21:29:26 +0530

    Word search implementation added

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/WordSearch.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/WordSearch.java
@@ -1,5 +1,26 @@
 package competitiveProgramming.leetcode;
 
+/*
+79. Word Search
+
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+Given word = "ABCCED", return true.
+Given word = "SEE", return true.
+Given word = "ABCB", return false.
+ */
+
 public class WordSearch {
     public static void main(String[] args) {
         char[][] arr = {
@@ -7,32 +28,33 @@ public class WordSearch {
                 {'S', 'F', 'C', 'S'},
                 {'A', 'D', 'E', 'E'}
         };
-        System.out.println(exist(arr, "abcd"));
+        System.out.println(exist(arr, "SEE"));
     }
 
     private static boolean exist(char[][] board, String word) {
-        int[] alphabetArray = new int[100];
-        int[] wordAlphabetArray = new int[100];
+        char[] arr = word.toCharArray();
 
-        for (char[] chars : board) {
-            formulateAlphabetArray(chars, alphabetArray);
-        }
-        formulateAlphabetArray(word.toCharArray(), wordAlphabetArray);
-        return checkFeasibility(wordAlphabetArray, alphabetArray);
-    }
-
-    private static boolean checkFeasibility(int[] wordAlphabetArray, int[] alphabetArray) {
-        for (int i = 0; i < alphabetArray.length; i++) {
-            if (alphabetArray[i] - wordAlphabetArray[i] < 0) {
-                return false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if ((word.charAt(0) == board[i][j]) && existHelper(board, arr, i, j, board.length, board[0].length, 0)) {
+                    return true;
+                }
             }
         }
-        return true;
+        return false;
     }
 
-    private static void formulateAlphabetArray(char[] chars, int[] alphabetArray) {
-        for (char aChar : chars) {
-            alphabetArray[aChar - 'A'] = ++alphabetArray[aChar - 'A'];
+    private static boolean existHelper(char[][] board, char[] arr, int i, int j, int rows, int columns, int index) {
+        if (index == arr.length - 1) {
+            return true;
         }
+
+        if (i < 0 || i >= rows || j < 0 || j >= columns || board[i][j] != arr[index]) return false;
+
+        return existHelper(board, arr, i + 1, j, rows, columns, index + 1) ||
+                existHelper(board, arr, i - 1, j, rows, columns, index + 1) ||
+                existHelper(board, arr, i, j + 1, rows, columns, index + 1) ||
+                existHelper(board, arr, i, j - 1, rows, columns, index + 1);
     }
+
 }
