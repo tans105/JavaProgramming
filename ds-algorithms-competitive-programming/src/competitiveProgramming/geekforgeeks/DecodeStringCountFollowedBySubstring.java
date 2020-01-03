01cb79d8e357c3dee6db933c60e0f9b472ad960a
Tanmay <tanmayawasthi105@gmail.com>
2020-01-03 20:51:15 +0530

    more problems

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/geekforgeeks/DecodeStringCountFollowedBySubstring.java
@@ -0,0 +1,79 @@
+package competitiveProgramming.geekforgeeks;
+
+
+/*
+https://www.geeksforgeeks.org/decode-string-recursively-encoded-count-followed-substring/
+
+Decode a string recursively encoded as count followed by substring
+An encoded string (s) is given, the task is to decode it. The pattern in which the strings are encoded is as follows.
+
+<count>[sub_str] ==> The substring 'sub_str'
+                      appears count times.
+Examples:
+
+Input : str[] = "1[b]"
+Output : b
+
+Input : str[] = "2[ab]"
+Output : abab
+
+Input : str[] = "2[a2[b]]"
+Output : abbabb
+
+Input : str[] = "3[b2[ca]]"
+Output : bcacabcacabcaca
+
+ */
+public class DecodeStringCountFollowedBySubstring {
+    public static void main(String[] args) {
+        System.out.println(decode("1[b]"));
+    }
+
+    private static String decode(String s) {
+        char[] arr = generateStringArary(s);
+        return decode(arr);
+    }
+
+    private static String decode(char[] arr) {
+        StringBuilder builder = new StringBuilder();
+        String temp = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            try {
+                int count = Integer.parseInt(String.valueOf(arr[i]));
+                String s = builder.toString();
+                for (int j = 0; j < count; j++) {
+                    temp += s;
+                }
+                builder = new StringBuilder();
+                builder.append(temp);
+                temp = "";
+            } catch (Exception e) {
+                builder.insert(0, arr[i]);
+            }
+        }
+        return builder.toString();
+    }
+
+    private static char[] generateStringArary(String s) {
+        String[] arr = s.split("]");
+        StringBuilder builder = new StringBuilder();
+
+        for (String str : arr) {
+            builder.append(str);
+        }
+
+        arr = builder.toString().split("\\[");
+
+        builder = new StringBuilder();
+        for (String str : arr) {
+            if (str != null && !str.equals("")) {
+                builder.append(str);
+            }
+
+        }
+
+        return builder.toString().toCharArray();
+    }
+
+
+}
--- ds-algorithms-competitive-programming/src/competitiveProgramming/geekforgeeks/NextGreaterElement.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/geekforgeeks/NextGreaterElement.java
@@ -3,6 +3,8 @@ package competitiveProgramming.geekforgeeks;
 import utils.ArrayUtils;
 
 /*
+https://www.geeksforgeeks.org/replace-every-element-with-the-greatest-on-right-side/
+
 Replace every element with the greatest element on right side
 
 Given an array of integers, replace every element with the next greatest element (greatest element on the right side) in the array. Since there is no element next to the last element, replace it with -1. For example, if the array is {16, 17, 4, 3, 5, 2}, then it should be modified to {17, 5, 5, 5, 2, -1}.
