f56bd5e0ae70d25e068687c0e4f160662e00003a
Tanmay <tanmayawasthi105@gmail.com>
2020-02-16 12:53:09 +0530

    Implementation of blocking queue using synchronized blocks

---
+++ ds-algorithms-competitive-programming/src/miniApplications/blockingqueue/BlockingQueue.java
@@ -0,0 +1,55 @@
+package miniApplications.blockingqueue;
+
+import utils.LoggingUtil;
+import java.util.ArrayList;
+
+public class BlockingQueue {
+    private ArrayList<Integer> list;
+    private static final int DEFAULT_SIZE = 5;
+    private int size;
+    final Object lock = new Object();
+
+    BlockingQueue(int size) {
+        list = new ArrayList<>();
+        this.size = size;
+    }
+
+    BlockingQueue() {
+        list = new ArrayList<>();
+        this.size = DEFAULT_SIZE;
+    }
+
+
+    public void enqueue(int value) throws InterruptedException {
+        synchronized (lock) {
+            while (list.size() == this.size) {
+                lock.wait();
+            }
+
+            list.add(value);
+            LoggingUtil.logNewLine("Value", value, "added", ", current size:", list.size());
+            lock.notifyAll();
+        }
+    }
+
+    public void dequeue() throws InterruptedException {
+        synchronized (lock) {
+            while (list.size() == 0) {
+                lock.wait();
+            }
+            lock.notifyAll();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int value = list.remove(0);
+            LoggingUtil.logNewLine("Value", value, "removed", ", current size:", list.size());
+        }
+    }
+
+    public String toString() {
+        return list.toString();
+    }
+}
---
+++ ds-algorithms-competitive-programming/src/miniApplications/blockingqueue/Runner.java
@@ -0,0 +1,37 @@
+package miniApplications.blockingqueue;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        BlockingQueue queue = new BlockingQueue(5);
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100000; i++) {
+                    try {
+                        queue.enqueue(i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        t1.start();
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100000; i++) {
+                    try {
+                        queue.dequeue();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        t2.start();
+    }
+}
