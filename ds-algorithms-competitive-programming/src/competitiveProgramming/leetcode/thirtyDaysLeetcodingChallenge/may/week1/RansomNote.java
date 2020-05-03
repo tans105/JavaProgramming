b081bc39245650fa2d953812facc6b9d84ee180f
Tanmay <tanmayawasthi105@gmail.com>
2020-05-03 13:27:36 +0530

    30 days leetcoding challenge, week1, problem 3

---
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/may/week1/RansomNote.java
@@ -0,0 +1,43 @@
+package competitiveProgramming.leetcode.thirtyDaysLeetcodingChallenge.may.week1;
+
+import utils.ArrayUtils;
+import utils.LoggingUtil;
+
+/*
+https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3318/
+
+Ransom Note
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+ */
+public class RansomNote {
+    public static void main(String[] args) {
+        System.out.println(canConstruct("a", "b")); // false
+        System.out.println(canConstruct("aa", "ab")); // false
+        System.out.println(canConstruct("aa", "aab")); // true
+        System.out.println(canConstruct(null, "aab")); // false
+        System.out.println(canConstruct(null, null)); // false
+        System.out.println(canConstruct("", "")); // true
+    }
+
+    private static boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote == null && magazine == null) return true;
+        if (ransomNote == null || magazine == null) return false;
+
+        int[] ransomMap = new int[26];
+
+        for (char c : ransomNote.toCharArray()) ransomMap[c - 'a']++;
+        for (char c : magazine.toCharArray()) if (ransomMap[c - 'a'] > 0) ransomMap[c - 'a']--;
+        for (int val : ransomMap) if (val > 0) return false;
+
+        return true;
+    }
+}
