9e580f08b2b36ebeef3343c92aa3bfd89790f3d4
Tanmay <tanmayawasthi105@gmail.com>
2020-05-28 17:05:21 +0530

    more problems

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/may/week4/PossibleBipartition.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/may/week4/PossibleBipartition.java
@@ -49,9 +49,9 @@ Note:
  */
 public class PossibleBipartition {
     public static void main(String[] args) {
-//        System.out.println(possibleBipartition(4, ArrayUtils.parse2DArray("[[1,2],[1,3],[2,4]]")));
-//        System.out.println(possibleBipartition(3, ArrayUtils.parse2DArray("[[1,2],[1,3],[2,3]]")));
-//        System.out.println(possibleBipartition(5, ArrayUtils.parse2DArray("[[1,2],[2,3],[3,4],[4,5],[1,5]]")));
+        System.out.println(possibleBipartition(4, ArrayUtils.parse2DArray("[[1,2],[1,3],[2,4]]")));
+        System.out.println(possibleBipartition(3, ArrayUtils.parse2DArray("[[1,2],[1,3],[2,3]]")));
+        System.out.println(possibleBipartition(5, ArrayUtils.parse2DArray("[[1,2],[2,3],[3,4],[4,5],[1,5]]")));
         System.out.println(possibleBipartition(10, ArrayUtils.parse2DArray("[[4,7],[4,8],[5,6],[1,6],[3,7],[2,5],[5,8],[1,2],[4,9],[6,10],[8,10],[3,6],[2,10],[9,10],[3,9],[2,3],[1,9],[4,6],[5,7],[3,8],[1,8],[1,7],[2,4]]")));
     }
 
