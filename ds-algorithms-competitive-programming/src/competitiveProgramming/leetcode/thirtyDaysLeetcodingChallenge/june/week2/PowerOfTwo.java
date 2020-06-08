f3dc6759a1fe7ca9bb34081924a66141db35ed2f
Tanmay <tanmayawasthi105@gmail.com>
2020-06-08 20:10:49 +0530

    30 days leetcoding challenge, week2, problem 1

--- ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/june/week2/PowerOfTwo.java
+++ ds-algorithms-competitive-programming/src/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/june/week2/PowerOfTwo.java
@@ -1,5 +1,30 @@
 package competitiveProgramming.leetcode.thirtyDaysLeetcodingChallenge.june.week2;
+/*
+https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3354/
 
+Power of Two
+
+Given an integer, write a function to determine if it is a power of two.
+
+Example 1:
+
+Input: 1
+Output: true
+Explanation: 20 = 1
+
+Example 2:
+
+Input: 16
+Output: true
+Explanation: 24 = 16
+
+Example 3:
+
+Input: 218
+Output: false
+
+
+ */
 public class PowerOfTwo {
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(-16));
