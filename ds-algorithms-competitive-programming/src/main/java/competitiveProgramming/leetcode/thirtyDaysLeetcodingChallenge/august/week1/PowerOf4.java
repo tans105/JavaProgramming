0623ed53a15d2a4c6680c3c820264812158bd420
Tanmay <tanmayawasthi105@gmail.com>
2020-08-04 13:03:06 +0530

    30 days leetcoding challenge, week1, problem 4

---
+++ ds-algorithms-competitive-programming/src/main/java/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/august/week1/PowerOf4.java
@@ -0,0 +1,40 @@
+package competitiveProgramming.leetcode.thirtyDaysLeetcodingChallenge.august.week1;
+
+/*
+https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3412/
+
+Power of Four
+
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+
+Example 2:
+
+Input: 5
+Output: false
+
+Follow up: Could you solve it without loops/recursion?
+ */
+public class PowerOf4 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(16));
+        System.out.println(isPowerOfFour(5));
+        System.out.println(isPowerOfFour(17));
+        System.out.println(isPowerOfFour(64));
+        System.out.println(isPowerOfFour(256));
+        System.out.println(isPowerOfFour(512));
+    }
+
+    public static boolean isPowerOfFour(int num) {
+        if(num == 1) return true;
+        if (num % 10 == 4 || num % 10 == 6) { //this eliminates power of 2 which are are not power of 4
+            return (num & (2 * num - 1)) == num;
+        } else {
+            return false;
+        }
+    }
+}
