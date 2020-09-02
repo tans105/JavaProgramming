cf456a2769e28ac9418e5bf9e56c1473cce6c392
Tanmay <tanmayawasthi105@gmail.com>
2020-09-02 13:50:48 +0530

    30 days leetcoding challenge, week1, problem 1

--- ds-algorithms-competitive-programming/src/main/java/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/september/week1/LargestTimeForGivenNumbers.java
+++ ds-algorithms-competitive-programming/src/main/java/competitiveProgramming/leetcode/thirtyDaysLeetcodingChallenge/september/week1/LargestTimeForGivenNumbers.java
@@ -3,6 +3,30 @@ package competitiveProgramming.leetcode.thirtyDaysLeetcodingChallenge.september.
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+https://leetcode.com/explore/featured/card/september-leetcoding-challenge/554/week-1-september-1st-september-7th/3445/
+
+Largest Time for Given Digits
+
+Given an array of 4 digits, return the largest 24 hour time that can be made.
+
+The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a time is larger if more time has elapsed since midnight.
+
+Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
+
+
+
+Example 1:
+
+Input: [1,2,3,4]
+Output: "23:41"
+
+Example 2:
+
+Input: [5,5,5,5]
+Output: ""
+
+ */
 public class LargestTimeForGivenNumbers {
     public static void main(String[] args) {
         System.out.println(largestTimeFromDigits(new int[]{1, 2, 3, 4}));
